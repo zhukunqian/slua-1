82cee85bd4c5b3bbaa65f421bb4037d3a3dbeff7
陆泽西 <jesse_luzexi@163.com>
2015-09-07 00:27:22 +0800

    fix bundle in mac

--- build/make_osx.sh
+++ build/make_osx.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env bash
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
-cd pbc
-make clean
-xcodebuild
-cp -f ./Build/Products/Release/libpbc.a ./Build/libpbc.a
-cd ..
-
 cd lua-5.1.5/slua
+xcodebuild clean
 xcodebuild
-cp -r ./build/Release/slua.bundle ../../../Assets/Plugins/
+cp -r build/Release/slua.bundle ../../../Assets/Plugins/
 
 cd -
