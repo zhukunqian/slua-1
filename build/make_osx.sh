ba502e95118b1398c819e8f3cd78fb6bb2826c5e
陆泽西 <jesse_luzexi@163.com>
2015-09-07 00:06:58 +0800

    fix make osx bundle

--- build/make_osx.sh
+++ build/make_osx.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
+cd pbc
+make clean
+xcodebuild
+cp -f ./Build/Products/Release/libpbc.a ./Build/libpbc.a
+cd ..
+
 cd lua-5.1.5/slua
 xcodebuild
-cp -r Build/Release/slua.bundle ../../../Assets/Plugins/
+cp -r ./build/Release/slua.bundle ../../../Assets/Plugins/
 
 cd -
---
+++ build/make_osxex.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+
+cd lua-5.1.5/slua
+xcodebuild clean
+xcodebuild
+cp -r build/Release/slua.bundle ../../../Assets/Plugins/
+
+cd -
