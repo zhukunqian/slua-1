3b7fb1a39337830e325ecb77b1b31a0bb0135c86
陆泽西 <jesse_luzexi@163.com>
2015-09-07 00:54:43 +0800

    fix

--- build/luajit-2.1.0/slua/build/slua.build/Release-iphoneos/slua.build/Objects-normal/armv7/slua.d
+++
@@ -1,5 +0,0 @@
-dependencies: \
-  /Users/luzexi/Desktop/work/github/slua/Assets/slua_src/slua.c \
-  /Users/luzexi/Desktop/work/github/slua/build/luajit-2.1.0/slua/../src/lua.h \
-  /Users/luzexi/Desktop/work/github/slua/build/luajit-2.1.0/slua/../src/luaconf.h \
-  /Users/luzexi/Desktop/work/github/slua/build/luajit-2.1.0/slua/../src/lauxlib.h
