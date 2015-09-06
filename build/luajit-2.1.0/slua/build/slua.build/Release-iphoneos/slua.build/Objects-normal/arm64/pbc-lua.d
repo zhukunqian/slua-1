a6139e6c034d5f1f4075d643c56cc6e365f295b6
陆泽西 <jesse_luzexi@163.com>
2015-09-06 17:56:11 +0800

    fix

--- build/luajit-2.1.0/slua/build/slua.build/Release-iphoneos/slua.build/Objects-normal/arm64/pbc-lua.d
+++
@@ -1,7 +0,0 @@
-dependencies: \
-  /Users/luzexi/Desktop/work/gitlab/arpg/client/Tool/luaMacBuild/pbc/binding/lua/pbc-lua.c \
-  /Users/luzexi/Desktop/work/gitlab/arpg/client/Tool/luaMacBuild/os/../luajit/src/lua.h \
-  /Users/luzexi/Desktop/work/gitlab/arpg/client/Tool/luaMacBuild/os/../luajit/src/luaconf.h \
-  /Users/luzexi/Desktop/work/gitlab/arpg/client/Tool/luaMacBuild/os/../luajit/src/lualib.h \
-  /Users/luzexi/Desktop/work/gitlab/arpg/client/Tool/luaMacBuild/os/../luajit/src/lauxlib.h \
-  /Users/luzexi/Desktop/work/gitlab/arpg/client/Tool/luaMacBuild/os/../pbc/pbc.h
--- build/luajit-2.1.0/slua/build/slua.build/Release-iphoneos/slua.build/Objects-normal/armv7/lua_wrap.d
+++
@@ -1,5 +0,0 @@
-dependencies: \
-  /Users/luzexi/Desktop/work/gitlab/arpg/client/Tool/luaMacBuild/lua_wrap.c \
-  /Users/luzexi/Desktop/work/gitlab/arpg/client/Tool/luaMacBuild/os/../luajit/src/lua.h \
-  /Users/luzexi/Desktop/work/gitlab/arpg/client/Tool/luaMacBuild/os/../luajit/src/luaconf.h \
-  /Users/luzexi/Desktop/work/gitlab/arpg/client/Tool/luaMacBuild/os/../luajit/src/lauxlib.h
