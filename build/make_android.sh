328d6bb0d1e33d291fa5989681f5fb85a21bca8e
陆泽西 <jesse_luzexi@163.com>
2015-09-07 10:00:59 +0800

    fix android lib build

--- build/make_android.sh
+++ build/make_android.sh
@@ -1,8 +1,8 @@
 
 cd android/jni
 ./build_arm_v7.sh
+cp ../libs/armeabi-v7a/libslua.so ../../../Assets/Plugins/Android/libs/armeabi-v7a/libslua.so
 ./build_x86.sh
+cp ../libs/x86/libslua.so ../../../Assets/Plugins/Android/libs/x86/libslua.so
 cd ../../
-cp ./android_lib/armeabi-v7a/libslua.so ../Assets/Plugins/Android/libs/armeabi-v7a/libslua.so
-cp ./android_lib/x86/libslua.so ../Assets/Plugins/Android/libs/x86/libslua.so
 
