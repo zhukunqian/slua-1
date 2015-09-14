fcb898b41e1014ea7327df4f40f0db87e1e7b2dc
luzexi <jesse_luzexi@163.com>
2015-09-14 11:14:41 +0800

    fix win build

--- build/make-windows-32.cmd
+++ build/make-windows-32.cmd
@@ -19,25 +19,25 @@ gcc ..\Assets\slua_src\slua.c ^
 	lua-cjson-2.1.0\strbuf.c ^
 	lua-cjson-2.1.0\lua_cjson.c ^
 	lua-cjson-2.1.0\fpconv.c ^
-	pbc-master\src\alloc.c ^
-	pbc-master\src\array.c ^
-	pbc-master\src\bootstrap.c ^
-	pbc-master\src\context.c ^
-	pbc-master\src\decode.c ^
-	pbc-master\src\map.c ^
-	pbc-master\src\pattern.c ^
-	pbc-master\src\proto.c ^
-	pbc-master\src\register.c ^
-	pbc-master\src\rmessage.c ^
-	pbc-master\src\stringpool.c ^
-	pbc-master\src\varint.c ^
-	pbc-master\src\wmessage.c ^
-	pbc-master\binding\lua\pbc-lua.c ^
+	pbc-win\src\alloc.c ^
+	pbc-win\src\array.c ^
+	pbc-win\src\bootstrap.c ^
+	pbc-win\src\context.c ^
+	pbc-win\src\decode.c ^
+	pbc-win\src\map.c ^
+	pbc-win\src\pattern.c ^
+	pbc-win\src\proto.c ^
+	pbc-win\src\register.c ^
+	pbc-win\src\rmessage.c ^
+	pbc-win\src\stringpool.c ^
+	pbc-win\src\varint.c ^
+	pbc-win\src\wmessage.c ^
+	pbc-win\binding\lua\pbc-lua.c ^
 	-o window/x86/slua.dll -m32 -shared ^
 	-I.\ ^
 	-Ilua-cjson-2.1.0 ^
 	-Iluajit-2.0.4\src ^
-	-Ipbc-master ^
+	-Ipbc-win ^
 	-Wl,--whole-archive window\x86\libluajit.a ^
 	-Wl,--no-whole-archive -lwsock32 -static-libgcc -static-libstdc++
 
