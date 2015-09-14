84ba7ed256c86692032766d3382e2ded0317b5e8
luzexi <jesse_luzexi@163.com>
2015-09-14 10:20:02 +0800

    fix windows build cmd

--- build/make-windows-32.cmd
+++ build/make-windows-32.cmd
@@ -13,7 +13,19 @@ mingw32-make BUILDMODE=static CC="gcc -m32" lib
 copy build\libpbc.a ..\window\x86\libpbc.a /y
 cd ..
 
-gcc ..\Assets\slua_src\slua.c lpeg.c pbc-win\binding\lua\pbc-lua.c lua-cjson-2.1.0\strbuf.c lua-cjson-2.1.0\lua_cjson.c lua-cjson-2.1.0\fpconv.c -o window/x86/slua.dll -m32 -shared -I.\ -Ilua-cjson-2.1.0 -Iluajit-2.0.4\src -Ipbc-win -Wl,--whole-archive window\x86\libluajit.a window\x86\libpbc.a -Wl,--no-whole-archive -lwsock32 -static-libgcc -static-libstdc++
+gcc ..\Assets\slua_src\slua.c ^
+	lpeg.c ^
+	pbc-win\binding\lua\pbc-lua.c ^
+	lua-cjson-2.1.0\strbuf.c ^
+	lua-cjson-2.1.0\lua_cjson.c ^
+	lua-cjson-2.1.0\fpconv.c ^
+	-o window/x86/slua.dll -m32 -shared ^
+	-I.\ ^
+	-Ilua-cjson-2.1.0 ^
+	-Iluajit-2.0.4\src ^
+	-Ipbc-win ^
+	-Wl,--whole-archive window\x86\libluajit.a window\x86\libpbc.a ^
+	-Wl,--no-whole-archive -lwsock32 -static-libgcc -static-libstdc++
 
 copy window\x86\slua.dll ..\Assets\Plugins\x86\slua.dll /y
 pause
