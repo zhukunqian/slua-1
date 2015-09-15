be1f79918b5f0b4b8f49e953e51ec2881ffdf199
陆泽西 <jesse_luzexi@163.com>
2015-09-15 11:42:09 +0800

    add luasocket

--- build/luasocket-2.0.2/src/luasocket.c
+++ build/luasocket-2.0.2/src/luasocket.c
@@ -99,6 +99,7 @@ static int base_open(lua_State *L) {
         lua_pushstring(L, "_VERSION");
         lua_pushstring(L, LUASOCKET_VERSION);
         lua_rawset(L, -3);
+        
         return 1;
     } else {
         lua_pushstring(L, "unable to initialize library");
