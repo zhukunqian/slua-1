b4262e0454b21254a01b32c634fd43de377a3f21
陆泽西 <jesse_luzexi@163.com>
2015-09-23 22:11:50 +0800

    rebuild android

---
+++ build/android/obj/local/x86/objs/slua/__/__/sproto-master/lsproto.o.d
@@ -0,0 +1,111 @@
+obj/local/x86/objs/slua/__/__/sproto-master/lsproto.o: \
+ jni/../../sproto-master/lsproto.c \
+ /Users/luzexi/Desktop/work/_Environment/android-ndk-r9/platforms/android-9/arch-x86/usr/include/string.h \
+ /Users/luzexi/Desktop/work/_Environment/android-ndk-r9/platforms/android-9/arch-x86/usr/include/sys/cdefs.h \
+ /Users/luzexi/Desktop/work/_Environment/android-ndk-r9/platforms/android-9/arch-x86/usr/include/sys/cdefs_elf.h \
+ /Users/luzexi/Desktop/work/_Environment/android-ndk-r9/platforms/android-9/arch-x86/usr/include/android/api-level.h \
+ /Users/luzexi/Desktop/work/_Environment/android-ndk-r9/platforms/android-9/arch-x86/usr/include/malloc.h \
+ /Users/luzexi/Desktop/work/_Environment/android-ndk-r9/platforms/android-9/arch-x86/usr/include/stdlib.h \
+ /Users/luzexi/Desktop/work/_Environment/android-ndk-r9/platforms/android-9/arch-x86/usr/include/alloca.h \
+ /Users/luzexi/Desktop/work/_Environment/android-ndk-r9/platforms/android-9/arch-x86/usr/include/strings.h \
+ /Users/luzexi/Desktop/work/_Environment/android-ndk-r9/platforms/android-9/arch-x86/usr/include/sys/types.h \
+ /Users/luzexi/Desktop/work/_Environment/android-ndk-r9/platforms/android-9/arch-x86/usr/include/stdint.h \
+ /Users/luzexi/Desktop/work/_Environment/android-ndk-r9/platforms/android-9/arch-x86/usr/include/sys/_types.h \
+ /Users/luzexi/Desktop/work/_Environment/android-ndk-r9/platforms/android-9/arch-x86/usr/include/machine/_types.h \
+ /Users/luzexi/Desktop/work/_Environment/android-ndk-r9/platforms/android-9/arch-x86/usr/include/linux/posix_types.h \
+ /Users/luzexi/Desktop/work/_Environment/android-ndk-r9/platforms/android-9/arch-x86/usr/include/linux/stddef.h \
+ /Users/luzexi/Desktop/work/_Environment/android-ndk-r9/platforms/android-9/arch-x86/usr/include/linux/compiler.h \
+ /Users/luzexi/Desktop/work/_Environment/android-ndk-r9/platforms/android-9/arch-x86/usr/include/asm/posix_types.h \
+ /Users/luzexi/Desktop/work/_Environment/android-ndk-r9/platforms/android-9/arch-x86/usr/include/asm/posix_types_32.h \
+ /Users/luzexi/Desktop/work/_Environment/android-ndk-r9/platforms/android-9/arch-x86/usr/include/asm/types.h \
+ /Users/luzexi/Desktop/work/_Environment/android-ndk-r9/platforms/android-9/arch-x86/usr/include/linux/types.h \
+ /Users/luzexi/Desktop/work/_Environment/android-ndk-r9/platforms/android-9/arch-x86/usr/include/machine/kernel.h \
+ /Users/luzexi/Desktop/work/_Environment/android-ndk-r9/platforms/android-9/arch-x86/usr/include/sys/sysmacros.h \
+ /Users/luzexi/Desktop/work/_Environment/android-ndk-r9/platforms/android-9/arch-x86/usr/include/memory.h \
+ jni/../../sproto-master/msvcint.h jni/luajit/src/lua.h \
+ jni/luajit/src/luaconf.h \
+ /Users/luzexi/Desktop/work/_Environment/android-ndk-r9/platforms/android-9/arch-x86/usr/include/limits.h \
+ /Users/luzexi/Desktop/work/_Environment/android-ndk-r9/platforms/android-9/arch-x86/usr/include/sys/limits.h \
+ /Users/luzexi/Desktop/work/_Environment/android-ndk-r9/platforms/android-9/arch-x86/usr/include/linux/limits.h \
+ /Users/luzexi/Desktop/work/_Environment/android-ndk-r9/platforms/android-9/arch-x86/usr/include/machine/internal_types.h \
+ /Users/luzexi/Desktop/work/_Environment/android-ndk-r9/platforms/android-9/arch-x86/usr/include/machine/limits.h \
+ /Users/luzexi/Desktop/work/_Environment/android-ndk-r9/platforms/android-9/arch-x86/usr/include/sys/syslimits.h \
+ /Users/luzexi/Desktop/work/_Environment/android-ndk-r9/platforms/android-9/arch-x86/usr/include/asm/page.h \
+ /Users/luzexi/Desktop/work/_Environment/android-ndk-r9/platforms/android-9/arch-x86/usr/include/asm/page_32.h \
+ jni/luajit/src/lauxlib.h \
+ /Users/luzexi/Desktop/work/_Environment/android-ndk-r9/platforms/android-9/arch-x86/usr/include/stdio.h \
+ jni/luajit/src/lua.h jni/../../sproto-master/sproto.h
+
+/Users/luzexi/Desktop/work/_Environment/android-ndk-r9/platforms/android-9/arch-x86/usr/include/string.h:
+
+/Users/luzexi/Desktop/work/_Environment/android-ndk-r9/platforms/android-9/arch-x86/usr/include/sys/cdefs.h:
+
+/Users/luzexi/Desktop/work/_Environment/android-ndk-r9/platforms/android-9/arch-x86/usr/include/sys/cdefs_elf.h:
+
+/Users/luzexi/Desktop/work/_Environment/android-ndk-r9/platforms/android-9/arch-x86/usr/include/android/api-level.h:
+
+/Users/luzexi/Desktop/work/_Environment/android-ndk-r9/platforms/android-9/arch-x86/usr/include/malloc.h:
+
+/Users/luzexi/Desktop/work/_Environment/android-ndk-r9/platforms/android-9/arch-x86/usr/include/stdlib.h:
+
+/Users/luzexi/Desktop/work/_Environment/android-ndk-r9/platforms/android-9/arch-x86/usr/include/alloca.h:
+
+/Users/luzexi/Desktop/work/_Environment/android-ndk-r9/platforms/android-9/arch-x86/usr/include/strings.h:
+
+/Users/luzexi/Desktop/work/_Environment/android-ndk-r9/platforms/android-9/arch-x86/usr/include/sys/types.h:
+
+/Users/luzexi/Desktop/work/_Environment/android-ndk-r9/platforms/android-9/arch-x86/usr/include/stdint.h:
+
+/Users/luzexi/Desktop/work/_Environment/android-ndk-r9/platforms/android-9/arch-x86/usr/include/sys/_types.h:
+
+/Users/luzexi/Desktop/work/_Environment/android-ndk-r9/platforms/android-9/arch-x86/usr/include/machine/_types.h:
+
+/Users/luzexi/Desktop/work/_Environment/android-ndk-r9/platforms/android-9/arch-x86/usr/include/linux/posix_types.h:
+
+/Users/luzexi/Desktop/work/_Environment/android-ndk-r9/platforms/android-9/arch-x86/usr/include/linux/stddef.h:
+
+/Users/luzexi/Desktop/work/_Environment/android-ndk-r9/platforms/android-9/arch-x86/usr/include/linux/compiler.h:
+
+/Users/luzexi/Desktop/work/_Environment/android-ndk-r9/platforms/android-9/arch-x86/usr/include/asm/posix_types.h:
+
+/Users/luzexi/Desktop/work/_Environment/android-ndk-r9/platforms/android-9/arch-x86/usr/include/asm/posix_types_32.h:
+
+/Users/luzexi/Desktop/work/_Environment/android-ndk-r9/platforms/android-9/arch-x86/usr/include/asm/types.h:
+
+/Users/luzexi/Desktop/work/_Environment/android-ndk-r9/platforms/android-9/arch-x86/usr/include/linux/types.h:
+
+/Users/luzexi/Desktop/work/_Environment/android-ndk-r9/platforms/android-9/arch-x86/usr/include/machine/kernel.h:
+
+/Users/luzexi/Desktop/work/_Environment/android-ndk-r9/platforms/android-9/arch-x86/usr/include/sys/sysmacros.h:
+
+/Users/luzexi/Desktop/work/_Environment/android-ndk-r9/platforms/android-9/arch-x86/usr/include/memory.h:
+
+jni/../../sproto-master/msvcint.h:
+
+jni/luajit/src/lua.h:
+
+jni/luajit/src/luaconf.h:
+
+/Users/luzexi/Desktop/work/_Environment/android-ndk-r9/platforms/android-9/arch-x86/usr/include/limits.h:
+
+/Users/luzexi/Desktop/work/_Environment/android-ndk-r9/platforms/android-9/arch-x86/usr/include/sys/limits.h:
+
+/Users/luzexi/Desktop/work/_Environment/android-ndk-r9/platforms/android-9/arch-x86/usr/include/linux/limits.h:
+
+/Users/luzexi/Desktop/work/_Environment/android-ndk-r9/platforms/android-9/arch-x86/usr/include/machine/internal_types.h:
+
+/Users/luzexi/Desktop/work/_Environment/android-ndk-r9/platforms/android-9/arch-x86/usr/include/machine/limits.h:
+
+/Users/luzexi/Desktop/work/_Environment/android-ndk-r9/platforms/android-9/arch-x86/usr/include/sys/syslimits.h:
+
+/Users/luzexi/Desktop/work/_Environment/android-ndk-r9/platforms/android-9/arch-x86/usr/include/asm/page.h:
+
+/Users/luzexi/Desktop/work/_Environment/android-ndk-r9/platforms/android-9/arch-x86/usr/include/asm/page_32.h:
+
+jni/luajit/src/lauxlib.h:
+
+/Users/luzexi/Desktop/work/_Environment/android-ndk-r9/platforms/android-9/arch-x86/usr/include/stdio.h:
+
+jni/luajit/src/lua.h:
+
+jni/../../sproto-master/sproto.h:
